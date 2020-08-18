47262ffe6fabb749ac41cfa03aa261a874409721
MatTerra <mateus.b.s.terra@gmail.com>
2020-08-17 22:11:24 -0300

    Controllers first version

--- Shop/Controllers/CategoryController.cs
+++ Shop/Controllers/CategoryController.cs
@@ -1,37 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 [Route("categories")]
 public class CategoryController : ControllerBase
 {
     [HttpGet]
     [Route("")]
-    public string Get()
+    public async Task<ActionResult<List<Category>>> Get()
     {
-
+        return new List<Category>();
     }
     [HttpGet]
     [Route("{id:int}")]
-    public string GetById(int id)
+    public async Task<ActionResult<Category>> GetById(int id)
     {
-
+        return new Category();
     }
     [HttpPut]
-    [Route("")]
-    public string Put()
+    [Route("{id:int}")]
+    public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model)
     {
+        if (model.Id == id)
+            return Ok(model);
 
+        return NotFound();
     }
     [HttpPost]
     [Route("")]
-    public string Post()
+    public async Task<ActionResult<Category>> Post([FromBody] Category model)
     {
-
+        return Ok(model);
     }
     [HttpDelete]
-    [Route("")]
-    public string Delete()
+    [Route("{id:int}")]
+    public async Task<ActionResult<List<Category>>> Delete()
     {
-
+        return Ok();
     }
 
 }
